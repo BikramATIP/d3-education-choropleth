403c2a59799b01393eff9b2d4a7aa480a30f8bad
Bikram Thapa <bikramt755@gmail>
2025-01-31 16:08:20 +1100

    retrieve the data

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,39 @@ const svgRef = useRef();
 const [eduData, setEduData] = useState(null)
 const [countyData, setCountyData] = useState(null)
 
+useEffect(() => {
+async function fetchData() {
+ try {
+  const [education, counties] = await Promise.all([
+    fetch('https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json')
+    .then(res => res.json()),
+    fetch('https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json')
+    .then(res => res.json())
+  ])
+  setEduData(education)
+  setCountyData(counties)
+ } catch (error) {
+   console.error("There was an errror fetching the data", error)
+ }
+}
+fetchData()
+console.log(eduData, countyData)
+}, [])
+
+useEffect(() => {
+if (!eduData || !countyData) return;
+
+const svg = d3.select(svgRef.current)
+
+
+
+
+
+},[eduData, countyData])
+
   return (
     <>
-       
+       <svg ref={svgRef} ></svg>
     </>
   )
 }
