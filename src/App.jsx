b2d40795fa89ef4cec067352e8d847fd0a29a621
Bikram Thapa <bikramt755@gmail>
2025-01-31 11:54:14 +1100

    remove default vite code and add txt containing json links

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { useState, useEffect, useRef } from 'react'
+import * as d3 from 'd3'
+
+
+function App() {
+const svgRef = useRef();
+const [eduData, setEduData] = useState(null)
+const [countyData, setCountyData] = useState(null)
+
+  return (
+    <>
+       
+    </>
+  )
+}
+
+export default App
